ebae9867516ed14f231ed0281841269d30222eb8
arranbdesign-ux <arranb.design@gmail.com>
2025-08-30 12:50:58 +0100

    Before changing dropdown style

--- js/script.js
+++ js/script.js
@@ -8,6 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
         { name: "Sword", image: "" }
     ];
 
+    const rarity = [
+        { name: "5 Star", image: "images/misc/5-star.png" },
+        { name: "4 Star", image: "images/misc/4-star.png" }
+    ];
+
+    const tier = [
+        { name: "SSS", image: "images/tiers/sss.png" },
+        { name: "SS", image: "images/tiers/ss.png" }
+    ];
+
     const weaponsByType = {
         Broadblade: [
             { name: "Lustrous Razor", atk: 587, atkp: 36.4, image: "" },
@@ -51,6 +61,8 @@ document.addEventListener("DOMContentLoaded", () => {
             background: "images/backgrounds/augusta-splash.webp",
             element: elements[1],
             weaponType: "Broadblade",
+            rarity: 0,
+            tier: 0,
         },
         {
             name: "Carlotta",
@@ -60,6 +72,8 @@ document.addEventListener("DOMContentLoaded", () => {
             background: "images/backgrounds/carlotta-splash.webp",
             element: elements[3],
             weaponType: "Pistols",
+            rarity: 0,
+            tier: 0,
         },
         {
             name: "Phrolova",
@@ -69,6 +83,8 @@ document.addEventListener("DOMContentLoaded", () => {
             background: "images/backgrounds/phrolova-splash.webp",
             element: elements[4],
             weaponType: "Rectifier",
+            rarity: 0,
+            tier: 1,
         },
         {
             name: "Shorekeeper",
@@ -78,15 +94,26 @@ document.addEventListener("DOMContentLoaded", () => {
             background: "images/backgrounds/shorekeeper-splash.webp",
             element: elements[5],
             weaponType: "Rectifier",
+            rarity: 0,
+            tier: 0,
         },
     ];
 
     const charNameElement = document.getElementById("charName");
+
     const characterDropdown = document.getElementById("characterDropdown");
+
     const bgImg = document.querySelector(".resonator-bg");
+
     const profileImg = document.querySelector(".details-profile");
-    const elementImg = document.querySelector(".details-icon");
-    const elementName = document.querySelector(".details-content h2");
+
+    const elementImg = document.querySelector("#resElem .details-icon");
+    const elementName = document.querySelector("#resElem h2");
+
+    const rarityImg = document.querySelector(".details-stars");
+
+    const tierImg = document.querySelector("#resTiers .details-icon");
+    const tierName = document.querySelector("#resTiers h2");
 
     // Helper function to convert hex to rgb
     function hexToRgb(hex) {
@@ -116,6 +143,16 @@ document.addEventListener("DOMContentLoaded", () => {
         elementImg.removeAttribute("alt");
         elementName.textContent = "-";       // placeholder text
 
+        tierImg.style.display = "none";
+        tierImg.removeAttribute("src"); // clears the image
+        tierImg.removeAttribute("alt");
+        tierName.textContent = "-";       // placeholder text
+
+        //Rarity
+        rarityImg.style.display = "none";
+        rarityImg.src = "";
+        rarityImg.alt = "No rarity";
+
         // Reset colors
         document.documentElement.style.setProperty("--color1", "#484848");
         document.documentElement.style.setProperty("--color2", "#373737");
@@ -143,6 +180,8 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         const char = resonators[charIndex];
+        const charRarity = rarity[char.rarity];
+        const charTier = tier[char.tier];
 
         // Background
         bgImg.classList.remove("placeholder");
@@ -163,8 +202,18 @@ document.addEventListener("DOMContentLoaded", () => {
         elementImg.style.display = "block";
         elementImg.src = char.element.image || ""; // Set image (empty if none)
         elementImg.alt = char.element.name || "Element";
-
         elementName.textContent = char.element.name || ""; // Set element name
+
+        //Rarity
+        rarityImg.style.display = "block";
+        rarityImg.src = charRarity.image;
+        rarityImg.alt = charRarity.name;
+
+        // Tier
+        tierImg.style.display = "block";
+        tierImg.src = charTier.image;
+        tierImg.alt = charTier.name;
+        tierName.textContent = charTier.name;
 
         // Update colors
         document.documentElement.style.setProperty("--color1", char.color1);
